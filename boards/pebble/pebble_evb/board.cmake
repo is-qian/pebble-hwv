e08dbbba9f8dc9bd681a23b180dc4790da5fdf63
Gerard Marull-Paretas <gerard@teslabs.com>
2024-10-16 14:53:55 +0200

    boards: pebble_evd: default to jlink runner
    JLink is far more reliable/stable than OpenOCD.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- boards/pebble/pebble_evb/board.cmake
+++ boards/pebble/pebble_evb/board.cmake
@@ -12,6 +12,6 @@ if(CONFIG_SOC_NRF5340_CPUNET_QKAA)
   board_runner_args(openocd --cmd-pre-init "targets nrf53.cpunet")
 endif()
 
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
