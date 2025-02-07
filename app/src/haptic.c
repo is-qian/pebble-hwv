35b976dcd0321432620888e0d36a522908d65f79
Gerard Marull-Paretas <gerard@teslabs.com>
2025-02-07 21:22:55 +0100

    app: add new shell to test haptics
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ app/src/haptic.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+#include <zephyr/shell/shell.h>
+
+#include <hwv/drivers/haptic.h>
+
+static const struct device *const haptic = DEVICE_DT_GET(DT_ALIAS(haptic0));
+
+static int cmd_haptic_configure(const struct shell *sh, size_t argc, char **argv)
+{
+	int err;
+	uint8_t ampl;
+
+	if (argc < 2) {
+		shell_error(sh, "Missing amplitude");
+		return -EINVAL;
+	}
+
+	ampl = strtoul(argv[1], NULL, 0);
+	if (ampl > 100U) {
+		shell_error(sh, "Amplitude must be less than or equal to 100");
+		return -EINVAL;
+	}
+
+	err = haptic_configure(haptic, ampl);
+	if (err < 0) {
+		shell_error(sh, "Failed to configure haptic (%d)", err);
+		return err;
+	}
+
+	shell_print(sh, "Haptic configured");
+
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_haptic_cmds,
+			       SHELL_CMD_ARG(configure, NULL, "Configure haptic",
+					     cmd_haptic_configure, 2, 0),
+			       SHELL_SUBCMD_SET_END);
+
+SHELL_SUBCMD_ADD((hwv), haptic, &sub_haptic_cmds, "Haptic", NULL, 0, 0);
